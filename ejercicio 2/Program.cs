a8d05629820c80b4949ed120e8d54971dd80999f
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-10 11:30:34 -0600

    ejercicio 2 terminado

--- ejercicio 2/Program.cs
+++ ejercicio 2/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de números que desea ingresar: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] numeros = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Ingrese el valor para cada posición [{i}]");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Ingrese el número que desea buscar en el arreglo: ");
+int valorBuscar = int.Parse(Console.ReadLine());
+
+int contador = 0;
+Console.WriteLine($"Buscando el valor {valorBuscar}...");
+
+for (int i = 0;i < n;i++)
+{
+if (numeros[i] == valorBuscar)
+    {
+        Console.WriteLine($"Coincidencia encontrada en el índice: {i}");
+        contador++;
+    }
+}
+
+if (contador > 0)
+{
+    Console.WriteLine($"El número {valorBuscar} aparece {contador} veces en el arreglo.");
+}
+else
+{
+    Console.WriteLine($"El número {valorBuscar} no se encuentra en el arreglo.");
+}
